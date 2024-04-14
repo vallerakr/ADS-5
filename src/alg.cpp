b03ffe465f0447ec5c6b576e63439285fcae1e4d
Kryazheva Valeria <150891739+vallerakr@users.noreply.github.com>
2024-04-14 19:47:07 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,12 +3,95 @@
 #include <map>
 #include "tstack.h"
 
+int prior(char x) {
+    switch (x) {
+        case '(': return 0;
+        case ')': return 1;
+        case '+':
+        case '-': return 2;
+        case '*':
+        case '/': return 3;
+        default: return -1;
+    }
+}
 std::string infx2pstfx(std::string inf) {
-  // добавьте код
-  return std::string("");
+    std::string num_str, num1_str;
+    TStack<char, 100> stack1;
+    for (auto& x : inf) {
+        int pr = prior(x);
+        if (pr == -1) {
+            num_str = num_str + x + ' ';
+        } else {
+            char znach = stack1.get();
+            if (pr == 0 || (prior(znach) < pr && stack1.isEmpty()) || x == ')') {
+                if (x == ')') {
+                    while (prior(znach) >= pr) {
+                        num_str = num_str + znach + ' ';
+                        stack1.pop();
+                        if (!stack1.isEmpty()) {
+                            znach = stack1.get();
+                        } else {
+                            break;
+                        }
+                    }
+                    stack1.pop();
+                } else {
+                    stack1.push(x);
+                }
+            } else {
+                while (prior(znach) >= pr) {
+                    num_str = num_str + znach + ' ';
+                    stack1.pop();
+
+                    if (!stack1.isEmpty()) {
+                        znach = stack1.get();
+                    } else {
+                        break;
+                    }
+                }
+                stack1.push(x);
+            }
+        }
+    }
+    while (!stack1.isEmpty()) {
+        num_str = num_str + stack1.get() + ' ';
+        stack1.pop();
+    }
+    for (int i = 0; i < num_str.size() - 1; i++) {
+        num1_str += num_str[i];
+    }
+    return num1_str;
 }
 
 int eval(std::string pref) {
-  // добавьте код
-  return 0;
+    std::string num_str = "";
+    TStack<int, 100> stack1;
+    for (int i = 0; i < pref.size(); ++i) {
+        if (prior(pref[i]) == -1) {
+            if (pref[i] == ' ') {
+                continue;
+            } else if (isdigit(pref[i + 1])) {
+                num_str += pref[i];
+                continue;
+            } else {
+                num_str += pref[i];
+                int value = atoi(num_str.c_str());
+                stack1.push(value);
+                num_str = "";
+            }
+        } else {
+            int b = stack1.pop();
+            int a = stack1.pop();
+            if (pref[i] == '+') {
+                stack1.push(a + b);
+            } else if (pref[i] == '-') {
+                stack1.push(a - b);
+            } else if (pref[i] == '*') {
+                stack1.push(a * b);
+            } else if (pref[i] == '/') {
+                stack1.push(a / b);
+            }
+        }
+    }
+    return stack1.get();
 }
